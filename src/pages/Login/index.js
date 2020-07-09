d220882d75266241051a68ba1d2ca5a2744b3e2c
Surya Keswani <35249458+sukeswan@users.noreply.github.com>
2020-07-09 07:31:00 -0700

    sign up update

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -10,7 +10,7 @@ firebase.auth().onAuthStateChanged(function(user) {
     if(user != null){
 
       var email_id = user.email;
-      document.getElementById("user_para").innerHTML = "Welcome User : " + email_id;
+      document.getElementById("user_para").innerHTML = "User email: " + email_id;
 
     }
 
@@ -40,26 +40,21 @@ function login(){
 
 }
 
-var provider = new firebase.auth.GoogleAuthProvider();
+function signup(){
 
-firebase.auth().signInWithPopup(provider).then(function(result) {
-    // This gives you a Google Access Token. You can use it to access the Google API.
-    var token = result.credential.accessToken;
-    // The signed-in user info.
-    var user = result.user;
-    // ...
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // The email of the user's account used.
-    var email = error.email;
-    // The firebase.auth.AuthCredential type that was used.
-    var credential = error.credential;
-    // ...
-  });
-  
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
 
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+
+      login; 
+
+}
 
 function logout(){
   firebase.auth().signOut();
