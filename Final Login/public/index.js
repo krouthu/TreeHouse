2cdff32174d5375da60372120b2284a7c27dc29a
Surya Keswani <35249458+sukeswan@users.noreply.github.com>
2020-07-20 17:46:29 -0700

    final login code
    with forwarding to renter and landlord sign up/detail pages

---
+++ Final Login/public/index.js
@@ -0,0 +1,73 @@
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    // User is signed in.
+    
+    document.getElementById("user_div").style.display = "block";
+    document.getElementById("login_div").style.display = "none";
+
+    var user = firebase.auth().currentUser;
+
+    if(user != null){
+
+      var email_id = user.email;
+      document.getElementById("user_para").innerHTML = "User email: " + email_id;
+
+    }
+
+  } else {
+    // No user is signed in.
+
+    document.getElementById("user_div").style.display = "none";
+    document.getElementById("login_div").style.display = "block";
+
+  }
+});
+
+function login(){
+
+  var userEmail = document.getElementById("email_field").value;
+  var userPass = document.getElementById("password_field").value;
+
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error : " + errorMessage);
+
+    // ...
+  });
+
+}
+
+function home(){
+  window.location='http://localhost:1234/';
+}
+
+function landlord(){
+  window.location='https://renter-profile2.web.app/';
+}
+
+function renter(){
+  window.location='https://renter-profile2.web.app/';
+}
+
+function signup(){
+
+    var userEmail = document.getElementById("email_field").value;
+    var userPass = document.getElementById("password_field").value;
+
+    firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });
+
+      login; 
+
+}
+
+function logout(){
+  firebase.auth().signOut();
+}
