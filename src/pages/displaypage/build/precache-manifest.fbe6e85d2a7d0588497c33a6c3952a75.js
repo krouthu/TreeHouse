c66b4bdacef3f789e23fe4789b3febb164fe9f80
Keerthana Routhu <krouthu@ucsc.edu>
2020-07-22 17:57:59 -0700

    display page deployed

---
+++ src/pages/displaypage/build/precache-manifest.fbe6e85d2a7d0588497c33a6c3952a75.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b866f8786b9c130afb00d6417c699ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f18d3f661bca1df33a02",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "aa32a584847322a972e3",
+    "url": "/static/css/main.f878814d.chunk.css"
+  },
+  {
+    "revision": "f18d3f661bca1df33a02",
+    "url": "/static/js/2.f4410c94.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/static/js/2.f4410c94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa32a584847322a972e3",
+    "url": "/static/js/main.0ef0a5e3.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "4b7cd9c60e9c882cc6d0d70ab62e82d2",
+    "url": "/static/media/picture.4b7cd9c6.jpg"
+  }
+]);
